391ac5d406e1116d6066bccef71d3ae0f5c0e420
Philipp Schiffmann <philippschiffmann@icloud.com>
2015-03-13 05:41:58 +0100

    Added license, setup.py, moved module contents to submodule of cre package.

---
+++ setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup
+
+setup(
+	name="cre",
+	packages=["cre"],
+	version="0.1.0",
+	author="Philipp Schiffmann",
+	author_email="philippschiffmann@icloud.com",
+	url="https://github.com/elaru/python3-cre",
+	description="A regular expression processor implemented in python.",
+	license="ISC",
+	classifiers=[
+		"Development Status :: 2 - Pre-Alpha",
+		"Intended Audience :: Developers",
+		"License :: OSI Approved :: ISC License (ISCL)",
+		"Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 3"
+	]
+)
